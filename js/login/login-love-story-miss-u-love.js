d45af705d403fbc3f3ab67678445d92ff07804e8
Md. Naiem Hossain <73810796+naiemhossain360@users.noreply.github.com>
2021-07-03 21:53:44 +0600

    Update

---
+++ js/login/login-love-story-miss-u-love.js
@@ -0,0 +1,13 @@
+
+var a=prompt("User Name");
+ var b=prompt("Password");
+  	if(a== "admin" && b=="4321")
+  	{
+  	    window.location.href="love-story-miss-u-love.html";
+  	}
+  	else
+  	{
+  		alert("Sorry Your User Name & Password Invalid");
+  		window.location.href="login-love-story-miss-u-love.html";
+  	}
+  	
